3f782cd7f80b2e0de658bb7bdb889459de7ee38c
jeremiah.1582@googlemail.com <jeremiah.1582@googlemail.com>
2022-10-19 12:56:26 +0200

    innitiate git repo

---
+++ myArray.js
@@ -0,0 +1,76 @@
+//pages listing jobs
+
+exports.myArray = [
+    {
+       name: "linkedin",
+       region: "europe",
+       link:"https://www.linkedin.com/jobs/search/?currentJobId=3306864994&f_WT=2&geoId=100506914&keywords=developer&location=Europe&refresh=true",
+       base:"https://www.linkedin.com/",
+    },
+   //  { //need to select a different element for this to work
+   //     name: "indeed",
+   //     region: "europe",
+   //     link:"https://de.indeed.com/jobs?q=all&l=Remote&vjk=94ac3a0aeba2370d",
+   //     base:"",
+   //  },
+    {
+       name: "stepstone",
+       region: "europe",
+       link:"https://www.stepstone.de/jobs/developer/in-remote?radius=30&q=developer+",
+       base:"",
+    },
+    {
+       name: "adzuna",
+        region: "europe",
+       link:"https://www.adzuna.de/search?q=software%20developer",
+       base:"",
+    },
+   // //  {//need to select a different element for this to work
+   // //     name: "bing",
+   // //      region: "europe",
+   // //     link:"https://www.bing.com/jobs?q=+developer+remote+jobs&go=Search&qs=ds&form=jobl2p&scp=0&c=1",
+   // //     base:"",
+   // //  },
+   // //  {//need to select a different element for this to work
+   // //     name: "remote.co",
+   // //      region: "europe",
+   // //     link:"https://remote.co/remote-jobs/developer/",
+   // //     base:"",
+   // //  },
+    {
+       name: "reed",
+        region: "UK",
+       link:"https://www.reed.co.uk/jobs/developer-jobs",
+       base:"https://www.reed.co.uk",
+    },
+    {
+       name: "totaljobs",
+        region: "UK",
+       link:"https://www.totaljobs.com/jobs/developer?radius=10",
+       base:"",
+    },
+    {
+       name: "universal jobmatch",
+        region: "UK",
+       link:"https://findajob.dwp.gov.uk/search?q=developer&w=",
+       base:"",
+    },
+   // //  {//need to select a different element for this to work
+   // //     name: "jobsite",
+   // //      region: "UK",
+   // //     link:"https://www.jobsite.co.uk/jobs/developer?radius=20",
+   // //     base:"",
+   // //  },
+   // //  {//NOT SURE WHY THIS OBJECT ISNT working 
+   // //     name: "monster",
+   // //      region: "UK",
+   // //     link:"https://www.monster.co.uk/jobs/search?q=developer&where=&page=1&so=m.h.s",
+   // //     base:"",
+   // //  },
+    {
+       name: "the guardian jobs",
+        region: "UK",
+       link:"https://jobs.theguardian.com/searchjobs/?Keywords=developer&radialtown=&LocationId=&RadialLocation=30",
+       base:"",
+    },
+  ];
---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const cheerio = require("cheerio");
+const PORT = process.env.PORT || 8001;
+const { myArray } = require("./myArray");
+
+const articles = [];
+const specificArticle = [];
+
+app.get("/", (req, res) => {
+  res.status(200).json("welcome to my JobListings API");
+});
+// this is your first scraping tool-complete
+
+app.get("/listings", async (req, res) => {
+  const keyword = "Developer";
+  const keyword2 = "Software";
+  const keyword3 = "Entwickler";
+  const keyword4 = "React";
+  myArray.forEach((item) => {
+    axios
+      .get(`${item.link}`)
+      .then((response) => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        // 1
+        $(`a:contains(${keyword || keyword2 || keyword3 || keyword4})`,html).each(function () { //arrow function wont work here
+         
+          const title = $(this).text();
+          const url = $(this).attr("href");
+
+          articles.push({
+            title,
+            url: item.base + url,
+            source: item.name,
+          });
+        });
+      })
+      .catch((err) => {
+        console.log("there was an error with you request...", err);
+      });
+  });
+  if (articles !== []) {
+    await res.status(200).json(articles);
+  }
+});
+app.get("/listings/:paramsId", (req, res) => {
+  const paramsId = req.params.paramsId;
+  const arrayItem = myArray.filter((item) => item.name == paramsId)[0];
+  const baseLink = myArray.filter((item) => item.name == paramsId)[0].base;
+
+  axios
+    .get(arrayItem.address)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      $(`a:contains(${keyword})`, html).each(function () {
+        const title = $(this).text();
+        const url = $(this).attr("href");
+        specificArticle.push({
+          title,
+          url: baseLink + url,
+          source: arrayItem.name,
+        });
+      });
+      res.status(200).json(specificArticle);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log("port is running on ", PORT);
+});
