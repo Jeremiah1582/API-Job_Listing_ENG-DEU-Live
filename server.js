1372ca38cdfbb88cabc36bc325d00c04ba1721ed
jeremiah.1582@googlemail.com <jeremiah.1582@googlemail.com>
2022-10-19 14:06:38 +0200

    structured the function outside of the route being called to increase the speed and eficiency at which the client recieves the results

--- server.js
+++ server.js
@@ -6,41 +6,37 @@ const PORT = process.env.PORT || 8001;
 const { myArray } = require("./myArray");
 
 const listings = [];
+const keyword = "Developer";
+const keyword2 = "Software";
+const keyword3 = "Entwickler";
+const keyword4 = "React";
+
+myArray.forEach((item) => {
+  axios
+    .get(`${item.link}`)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      $(`a:contains(${keyword || keyword2 || keyword3 || keyword4})`,html).each(function () {//arrow function wont work here
+        const title = $(this).text();
+        const url = $(this).attr("href");
+
+        listings.push({
+          title,
+          url: item.base + url,
+          source: item.name,
+        });
+      });
+    })
+    .catch((err) => {
+      console.log("there was an error with you request...", err);
+    });
+   
+});
 
 app.get("/", async (req, res) => {
-  const keyword = "Developer";
-  const keyword2 = "Software";
-  const keyword3 = "Entwickler";
-  const keyword4 = "React";
-  myArray.forEach((item) => {
-    axios
-      .get(`${item.link}`)
-      .then((response) => {
-        const html = response.data;
-        const $ = cheerio.load(html);
-        // 1
-        $(
-          `a:contains(${keyword || keyword2 || keyword3 || keyword4})`,
-          html
-        ).each(function () {
-          //arrow function wont work here
-
-          const title = $(this).text();
-          const url = $(this).attr("href");
-
-          listings.push({
-            title,
-            url: item.base + url,
-            source: item.name,
-          });
-        });
-      })
-      .catch((err) => {
-        console.log("there was an error with you request...", err);
-      });
-  });
   if (listings !== []) {
-    await res.status(200).json(listings);
+      res.status(200).json(listings);
   }
 });
 
@@ -48,4 +44,3 @@ app.listen(PORT, () => {
   console.log("port is running on ", PORT);
 });
 
-module.exports=listings
